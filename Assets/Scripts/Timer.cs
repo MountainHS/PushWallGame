8e76a24a8a455918de06a2d572a61db014806177
MountainHS <hsgood01@gmail.com>
2020-08-29 01:17:35 +0900

    5. 게임 시작하고 끝낼 때 시간 재기(다시 시작하면 초기화) 마무리
    Timer 텍스트 오브젝트에 Timer 스크립트를 넣어서 걸린 시간을 소숫점
    2자리에서 잘라서 나타나도록 했음.
    게임클리어시 걸린 시간 멈추는건 접근 제한자가 public인 bool 형 변수
    gameClear가 담당함. (True이면 멈추고, False이면 계속 시간재게 만들어
    놧음 - 당연히 기본값은 False임.)

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public Text timeText;
+    float time = 0f;
+    public bool gameClear = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!gameClear)
+        {
+            time += Time.deltaTime;
+            string timeString = time.ToString("N2");
+            timeText.text = "걸린 시간: " + timeString;
+        }
+    }
+}
