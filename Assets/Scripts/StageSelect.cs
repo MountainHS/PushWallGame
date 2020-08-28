81c3daf8f9861bc5ac75fb5711cfea5758a51a2c
MountainHS <hsgood01@gmail.com>
2020-08-29 03:16:04 +0900

    6-1 완료, 6-2-a에서 스피드런 모드 아닐때 소스테이지 선택 화면으로 넘어가는 것 까지 만듬.

---
+++ Assets/Scripts/StageSelect.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StageSelect : MonoBehaviour
+{
+    public void SelectStageA()
+    {
+        SceneManager.LoadScene("SelectSubStageA");
+    }
+    public void SelectStageB()
+    {
+        SceneManager.LoadScene("SelectSubStageB");
+    }
+}
