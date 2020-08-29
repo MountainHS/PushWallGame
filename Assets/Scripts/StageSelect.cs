f2cc1ba9df5110229aa9170e78d1637c434694b7
MountainHS <hsgood01@gmail.com>
2020-08-29 18:15:35 +0900

    6-2, 6-2-a-1 완료. 스테이지 A,B 선택하면 스피드런 유무를 물어보는 텍스트가 뜸 (Y,N 으로 결정 가능함) 스피드런모드 선택하면 소스테이지 1로 이동 선택안하면 소스테이지 선택 씬으로 이동함.

--- Assets/Scripts/StageSelect.cs
+++ Assets/Scripts/StageSelect.cs
@@ -5,12 +5,45 @@ using UnityEngine.SceneManagement;
 
 public class StageSelect : MonoBehaviour
 {
+    public GameObject buttonStageA;
+    public GameObject buttonStageB;
+    public GameObject speedRun;
+    public bool enableSpeedrun = false;
+    bool stageAOn = false, stageBOn = false;
+
     public void SelectStageA()
     {
-        SceneManager.LoadScene("SelectSubStageA");
+        buttonStageA.SetActive(false);
+        buttonStageB.SetActive(false);
+        speedRun.SetActive(true);
+        stageAOn = true;
     }
     public void SelectStageB()
     {
-        SceneManager.LoadScene("SelectSubStageB");
+        buttonStageA.SetActive(false);
+        buttonStageB.SetActive(false);
+        speedRun.SetActive(true);
+        stageBOn = true;
+    }
+
+    void Update()
+    {
+        if (stageAOn)
+        {
+            if (Input.GetKey(KeyCode.N) == true)
+                SceneManager.LoadScene("SelectSubStageA");
+
+            if (Input.GetKey(KeyCode.Y) == true)
+                SceneManager.LoadScene("A-1");
+        }
+
+        if (stageBOn)
+        {
+            if (Input.GetKey(KeyCode.N) == true)
+                SceneManager.LoadScene("SelectSubStageB");
+
+            if (Input.GetKey(KeyCode.Y) == true)
+                SceneManager.LoadScene("B-1");
+        }
     }
 }
