b05aff0b59f8e2f62a9a00098b4713ddee7e3a11
MountainHS <hsgood01@gmail.com>
2020-08-31 23:15:28 +0900

    스테이지 매니저 스크립트 변경 (3*3에서 7*7로) 크기 변경된 스테이지 이용하려면 샘플씬 복사하거나 다른이름으로 저장할것.

--- Assets/Scripts/StageManager.cs
+++ Assets/Scripts/StageManager.cs
@@ -7,7 +7,7 @@ public class StageManager : MonoBehaviour
     void Start()
     {
         // VBN: 세로 길이, HBN: 가로 길이 저장 변수
-        float VBN = 3.0f, HBN = 3.0f;
+        float VBN = 7.0f, HBN = 7.0f;
 
         transform.localScale = new Vector3(HBN * 0.1f, 1, VBN * 0.1f);  // 세로, 가로 길이 만큼 맵 크기 변화
     }
