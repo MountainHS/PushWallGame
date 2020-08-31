5ea70f1acb7744763d37de55e4747c06f3936550
Last-asffs <38125012+Last-asffs@users.noreply.github.com>
2020-09-01 03:14:52 +0900

    3번 목표지점 도달시 스테이지이동(스피드런 구분해서)

---
+++ Assets/Scripts/NextStage.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextStage : MonoBehaviour
+{
+    public GameObject timerObject;
+    public Timer timerScript;
+    public void Start()
+    {
+        timerScript = timerObject.GetComponent<Timer>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "goal")
+        {
+            if (PlayerPrefs.GetInt("enableSpeedrun") == 1 && PlayerPrefs.GetInt("currentStage") != 11))
+            {
+                timerScript.gameClear = true;
+            }
+            SceneManager.LoadScene("SampleScene");
+        }
+    }
+}
